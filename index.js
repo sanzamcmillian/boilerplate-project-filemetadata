26473813c1e83be6f94e768c7cf2ab22937aa755
sanzamcmillian <g18s2658@ru.ac.za>
2025-08-17 14:04:13 +0200

    file lee meta

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var fs = require('fs');
+var path = require('path');
+const multer = require('multer');
 
 var app = express();
+
+const uploadsDir = path.join(process.cwd(), "uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +30,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
